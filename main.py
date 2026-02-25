2b0ea25d369ab52f1647b14ec17502e4c2b5a865
Abeeha-Nadeem <abeehanadeem006@gmail.com>
2026-02-26 01:04:45 +0500

    update CORS

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://report2-resolve-frontend.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
